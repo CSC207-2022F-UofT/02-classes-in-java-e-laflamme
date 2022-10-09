49e94afd846932a0313a121697225d91cd417f8c
emilycoffen <emily.coffen@outlook.com>
2022-10-09 15:19:19 -0400

    Fixed error in Task 2

--- src/main/java/Silly.java
+++ src/main/java/Silly.java
@@ -124,7 +124,7 @@ public class Silly implements Comparable<Silly>{
         y.countStatic();
         x.countStatic();
         x.countStatic();
-        int[] expected_values = {1, 1, 2, 3};
+        int[] expected_values = {1, 2, 3, 4};
 
         System.out.println("The countStatic calls will return " + Arrays.toString(expected_values));
     }
